06de7d7907e18bb21461b237cae877fd1f534425
HynekSvoboda <97514770+HynekSvoboda@users.noreply.github.com>
2023-10-02 11:09:55 +0200

    Add files via upload

---
+++ Test2.10/Test2.10/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Test2._10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(311, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "proveď";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(18, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(141, 225);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(188, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(141, 225);
+            this.listBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 252);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = ".";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 355);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Test2.10/Test2.10/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test2._10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader cteni = null;
+            StreamWriter prepis = null;
+            List<string> list = new List<string>();
+            try
+            {
+                listBox1.Items.Clear();
+                listBox2.Items.Clear();
+                cteni = new StreamReader("matematika.txt");
+                double soucet = 0;
+                int pocet = 0;
+                double prumer = 0;
+                while (!cteni.EndOfStream)
+                {
+                    string line = cteni.ReadLine();
+                    listBox1.Items.Add(line);
+                    string[] pole = line.Split(' ');
+                    int cislo1 = Convert.ToInt32(pole[0]);
+                    char znak = Convert.ToChar(pole[1]);
+                    int cislo2 = Convert.ToInt32(pole[2]);
+                    double vysledek = 0;
+                    switch(znak)
+                    {
+                        case '-':
+                            {
+                                vysledek = cislo1 - cislo2;
+                                pocet++;
+                                break;
+                            }
+                        case '+':
+                            {
+                                vysledek = cislo1 + cislo2;
+                                pocet++;
+                                break;
+                            }
+                        case '*':
+                            {
+                                vysledek = cislo1 * cislo2;
+                                pocet++;
+                                break;
+                            }
+                        case '/':
+                            {
+                                if (cislo2 == 0) break;
+                                checked { vysledek = (double)cislo1 /(double)cislo2; };
+                                pocet++;
+                                break;
+                            }
+                        default:
+                            {
+                                MessageBox.Show("Špatně zadaná operace");
+                                break;
+                            }
+                    }
+                    soucet += vysledek;
+                    string priklad = pole[0] +" "+ pole[1]+" " + pole[2]+" " + pole[3]+" " + vysledek;
+                    list.Add(priklad);
+                }
+                cteni.Close();
+
+                prepis = new StreamWriter("matematika.txt", false);
+                foreach (string s in list)
+                {
+                    prepis.WriteLine(s);
+                }
+                prepis.Close();
+
+                cteni = new StreamReader("matematika.txt");
+                while (!cteni.EndOfStream)
+                {
+                    listBox2.Items.Add(cteni.ReadLine());
+                }
+                prumer = soucet / pocet;
+                cteni.Close();
+
+                label1.Text = " prumer vysledku: " + prumer;
+
+                FileStream tok = new FileStream("prumer.dat", FileMode.OpenOrCreate, FileAccess.Write);
+                BinaryWriter zapisprum = new BinaryWriter(tok);
+                zapisprum.Write(prumer);
+                tok.Close();
+            }
+            catch(FileNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch(FormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch(OverflowException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch(DivideByZeroException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch(ArithmeticException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch(ArgumentOutOfRangeException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (cteni != null) cteni.Close();
+                if(prepis != null) prepis.Close();
+            }
+        }
+    }
+}
